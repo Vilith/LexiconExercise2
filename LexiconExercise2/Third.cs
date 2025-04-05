2071584a95b9245a1ef726c195017601204bdc22
Vilith <vilith44@gmail.com>
2025-04-05 21:17:00 +0200

    - Removed "testcode" that has served it's purpose. - Added comment to if-statement in "Third.cs"

--- LexiconExercise2/Third.cs
+++ LexiconExercise2/Third.cs
@@ -15,7 +15,7 @@ namespace LexiconExercise2
             string[] thirdWord = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
 
-            //Checking if 
+            //Checking if the stringarray contains 3 words separated with whitespace
             if (thirdWord.Length >= 3)
             {
                 Console.WriteLine($"{Environment.NewLine}You wrote: {input}" + 
@@ -25,34 +25,7 @@ namespace LexiconExercise2
             {
 
                 Console.WriteLine("Sentence shorter than 3 words");
-            }
-/*
-            //This snippet is a draft for testing if it works.
-            //Refactor when it works.
-
-            //Test less than 3 words
-            //string testString1 = "These words";
-            //string[] testArray = testString1.Split(" ");
-
-            //Test exactly 3 words
-            //string testString2 = "These three words";
-            //string[] testArray = testString2.Split(" ");
-
-            //Test more than 3 words
-            string testString3 = "This texts purpose is for testing";            
-            string[] testArray = testString3.Split(" ");
-
-            if (testArray.Length >= 3)
-            {
-                Console.WriteLine($"The third word is: {testArray[2]}");
-            } 
-            else
-            {
-                Console.WriteLine("This sentence does not contain enough word to extract the third word.");
-            }
-*/
-
-           
+            }           
         }
     }
 }
